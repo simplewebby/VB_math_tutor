e28146b554e96914fe260b632c406abfcb7d4879
Tsagan Garyaeva <tsagan81@gmail.com>
2017-12-29 15:44:25 -0500

    VB lab

--- Lab5mathTutor/Form1.Designer.vb
+++ Lab5mathTutor/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,186 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.radioDiv = New System.Windows.Forms.RadioButton()
+        Me.radioMult = New System.Windows.Forms.RadioButton()
+        Me.radioSub = New System.Windows.Forms.RadioButton()
+        Me.radioAdd = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblCorrectAnswer = New System.Windows.Forms.Label()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.btnCheckYourAnswer = New System.Windows.Forms.Button()
+        Me.txtUserInput = New System.Windows.Forms.TextBox()
+        Me.lblNumbers = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.AutoSize = True
+        Me.GroupBox1.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.GroupBox1.Controls.Add(Me.radioDiv)
+        Me.GroupBox1.Controls.Add(Me.radioMult)
+        Me.GroupBox1.Controls.Add(Me.radioSub)
+        Me.GroupBox1.Controls.Add(Me.radioAdd)
+        Me.GroupBox1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.GroupBox1.Location = New System.Drawing.Point(36, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(5)
+        Me.GroupBox1.Size = New System.Drawing.Size(531, 90)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Choose An Integer Operation"
+        '
+        'radioDiv
+        '
+        Me.radioDiv.AutoSize = True
+        Me.radioDiv.Location = New System.Drawing.Point(429, 37)
+        Me.radioDiv.Name = "radioDiv"
+        Me.radioDiv.Size = New System.Drawing.Size(85, 25)
+        Me.radioDiv.TabIndex = 3
+        Me.radioDiv.TabStop = True
+        Me.radioDiv.Text = "Division"
+        Me.radioDiv.UseVisualStyleBackColor = True
+        '
+        'radioMult
+        '
+        Me.radioMult.AutoSize = True
+        Me.radioMult.Location = New System.Drawing.Point(275, 37)
+        Me.radioMult.Name = "radioMult"
+        Me.radioMult.Size = New System.Drawing.Size(134, 25)
+        Me.radioMult.TabIndex = 2
+        Me.radioMult.TabStop = True
+        Me.radioMult.Text = "Multiplication"
+        Me.radioMult.UseVisualStyleBackColor = True
+        '
+        'radioSub
+        '
+        Me.radioSub.AutoSize = True
+        Me.radioSub.Location = New System.Drawing.Point(136, 37)
+        Me.radioSub.Name = "radioSub"
+        Me.radioSub.Size = New System.Drawing.Size(120, 25)
+        Me.radioSub.TabIndex = 1
+        Me.radioSub.TabStop = True
+        Me.radioSub.Text = "Subtraction"
+        Me.radioSub.UseVisualStyleBackColor = True
+        '
+        'radioAdd
+        '
+        Me.radioAdd.AutoSize = True
+        Me.radioAdd.Location = New System.Drawing.Point(23, 37)
+        Me.radioAdd.Name = "radioAdd"
+        Me.radioAdd.Size = New System.Drawing.Size(96, 25)
+        Me.radioAdd.TabIndex = 0
+        Me.radioAdd.TabStop = True
+        Me.radioAdd.Text = "Addition"
+        Me.radioAdd.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.AutoSize = True
+        Me.GroupBox2.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.GroupBox2.Controls.Add(Me.lblCorrectAnswer)
+        Me.GroupBox2.Controls.Add(Me.btnNext)
+        Me.GroupBox2.Controls.Add(Me.btnCheckYourAnswer)
+        Me.GroupBox2.Controls.Add(Me.txtUserInput)
+        Me.GroupBox2.Controls.Add(Me.lblNumbers)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.GroupBox2.Location = New System.Drawing.Point(36, 108)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(5)
+        Me.GroupBox2.Size = New System.Drawing.Size(531, 162)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        '
+        'lblCorrectAnswer
+        '
+        Me.lblCorrectAnswer.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.lblCorrectAnswer.Location = New System.Drawing.Point(21, 111)
+        Me.lblCorrectAnswer.Name = "lblCorrectAnswer"
+        Me.lblCorrectAnswer.Size = New System.Drawing.Size(286, 23)
+        Me.lblCorrectAnswer.TabIndex = 5
+        '
+        'btnNext
+        '
+        Me.btnNext.BackColor = System.Drawing.Color.LightBlue
+        Me.btnNext.Location = New System.Drawing.Point(332, 102)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(180, 32)
+        Me.btnNext.TabIndex = 4
+        Me.btnNext.Text = "Next Problem"
+        Me.btnNext.UseVisualStyleBackColor = False
+        '
+        'btnCheckYourAnswer
+        '
+        Me.btnCheckYourAnswer.BackColor = System.Drawing.Color.LightBlue
+        Me.btnCheckYourAnswer.Location = New System.Drawing.Point(332, 62)
+        Me.btnCheckYourAnswer.Name = "btnCheckYourAnswer"
+        Me.btnCheckYourAnswer.Size = New System.Drawing.Size(180, 34)
+        Me.btnCheckYourAnswer.TabIndex = 3
+        Me.btnCheckYourAnswer.Text = "Check Your Answer"
+        Me.btnCheckYourAnswer.UseVisualStyleBackColor = False
+        '
+        'txtUserInput
+        '
+        Me.txtUserInput.Location = New System.Drawing.Point(209, 62)
+        Me.txtUserInput.Name = "txtUserInput"
+        Me.txtUserInput.Size = New System.Drawing.Size(98, 27)
+        Me.txtUserInput.TabIndex = 2
+        '
+        'lblNumbers
+        '
+        Me.lblNumbers.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.lblNumbers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNumbers.Location = New System.Drawing.Point(25, 62)
+        Me.lblNumbers.Name = "lblNumbers"
+        Me.lblNumbers.Size = New System.Drawing.Size(167, 27)
+        Me.lblNumbers.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(21, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(386, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Please type in your answer to the following math problem. Integer answera only."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(602, 281)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Integer Math Tutor"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents radioDiv As RadioButton
+    Friend WithEvents radioMult As RadioButton
+    Friend WithEvents radioSub As RadioButton
+    Friend WithEvents radioAdd As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnNext As Button
+    Friend WithEvents btnCheckYourAnswer As Button
+    Friend WithEvents txtUserInput As TextBox
+    Friend WithEvents lblNumbers As Label
+    Friend WithEvents Label1 As Label
+    Private WithEvents lblCorrectAnswer As Label
 End Class
--- Lab5mathTutor/Form1.vb
+++ Lab5mathTutor/Form1.vb
@@ -1,3 +1,133 @@
-﻿Public Class Form1
+﻿'Tsagan Garyaeva
+'Comp-185
+'Lab 5 Math Tutor
 
+Public Class Form1
+    Dim rand As New Random
+    Dim intResult, useranswer, num1, num2 As Integer
+    Dim strOperator As String
+    Public Property Green As Color = Green
+    Public Property Red As Color = Red
+
+    Private Sub btnCheckResult_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+
+
+
+    Private Sub txtResult_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btnNextProblem_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub lblNum1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub lblOperator_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles radioDiv.CheckedChanged
+        strOperator = "/ "
+        'Loop to find a second operand that divides the first one evenly
+        Do While (num1 Mod num2 <> 0)
+            num1 = rand.Next(100) + 20
+            num2 = rand.Next(100) + 20
+        Loop
+        lblNumbers.Text = CStr(num1) & " " & strOperator & " " & CStr(num2)
+        intResult = num1 \ num2
+        txtUserInput.Focus()
+
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+
+        radioAdd.Checked = False
+        radioSub.Checked = False
+        radioMult.Checked = False
+        radioDiv.Checked = False
+        txtUserInput.Clear()
+        txtUserInput.Focus()
+
+        txtUserInput.Clear()
+        lblCorrectAnswer.Text = " "
+        lblNumbers.Text = " "
+        Randomize()
+        num1 = rand.Next(100) + 20
+        num2 = rand.Next(100) + 20
+
+
+    End Sub
+
+    Private Sub btnCheckYourAnswer_Click(sender As Object, e As EventArgs) Handles btnCheckYourAnswer.Click
+        Try
+
+            If (txtUserInput.Text = String.Empty) Then
+                MessageBox.Show("Please enter the answer")
+                txtUserInput.Focus()
+                'Check if the answer is an integer
+            ElseIf (Integer.TryParse(txtUserInput.Text, intResult)) Then
+                If (intResult = txtUserInput.Text) Then
+                    txtUserInput.BackColor = Green
+                    lblCorrectAnswer.Text = "Correct!"
+
+
+                Else
+                    lblCorrectAnswer.Text = "Incorrect, Please Try again"
+                    txtUserInput.BackColor = Red
+                    txtUserInput.Clear()
+
+                End If
+            Else
+                MessageBox.Show("Invalid input, Try again")
+                txtUserInput.Clear()
+
+            End If
+        Catch
+        End Try
+
+
+    End Sub
+
+    Private Sub lblCorrectAnswer_Click(sender As Object, e As EventArgs) Handles lblCorrectAnswer.Click
+
+    End Sub
+
+    Private Sub radioAdd_CheckedChanged(sender As Object, e As EventArgs) Handles radioAdd.CheckedChanged
+        strOperator = "+"
+        lblNumbers.Text = CStr(num1) & " " & strOperator & " " & CStr(num2)
+        intResult = num1 + num2
+        txtUserInput.Focus()
+
+    End Sub
+
+    Private Sub radioSub_CheckedChanged(sender As Object, e As EventArgs) Handles radioSub.CheckedChanged
+        strOperator = "-"
+        lblNumbers.Text = CStr(num1) & " " & strOperator & " " & CStr(num2)
+        intResult = num1 - num2
+        txtUserInput.Focus()
+    End Sub
+
+    Private Sub radioMult_CheckedChanged(sender As Object, e As EventArgs) Handles radioMult.CheckedChanged
+        strOperator = "*"
+        lblNumbers.Text = CStr(num1) & " " & strOperator & " " & CStr(num2)
+        intResult = num1 * num2
+        txtUserInput.Focus()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        num1 = rand.Next(100) + 20
+        num2 = rand.Next(100) + 20
+
+    End Sub
 End Class
